a408332333fd9dc9c9f0261560165a2070480ff6
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-10-01 10:41:24 -0600

    hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Route } from "wouter";
+import { Route, Router } from "wouter";
 import "./App.css";
 import Login from "./pages/Login";
+import { useHashLocation } from "wouter/use-hash-location";
 
 function App() {
   return (
-    <>
+    <Router hook={useHashLocation}>
       <Route path="/login" component={Login} />
-    </>
+    </Router>
   );
 }
 
